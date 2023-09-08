e95ef712f3539716732b54a7194788e4ce0c9646
Github <emyy5>
2023-09-08 06:34:33 +0300

    second

--- LifeCycleViewController/ViewController.swift
+++ LifeCycleViewController/ViewController.swift
@@ -12,18 +12,23 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        print("viewDidLoad")
     }
     override func viewWillAppear(_ animated: Bool) {
         // screen hatezhar now
+        print("viewWillAppear")
     }
     override func viewDidAppear(_ animated: Bool) {
        // zaharet already
+        print("viewDidAppear")
     }
     override func viewWillDisappear(_ animated: Bool) {
         //screen feha pass and email w b3daha hyro7 3la el homePage
+        print("viewWillDisappear")
     }
     override func viewDidDisappear(_ animated: Bool) {
         //ra7 3la el homePage already
+        print("viewDidDisappear")
     }
 
 
